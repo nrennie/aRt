e1c87bb655b365accc0f26056f6f5bc3b5ff6bc9
Nicola Rennie <nrennie35@gmail.com>
2021-10-08 23:52:00 +0100

    edit roxygen imports

--- R/circular.R
+++ R/circular.R
@@ -7,6 +7,10 @@
 #' @param bg_col Background colour. Default white.
 #' @param s Seed value. Default 56.
 #' @return A ggplot object.
+#' @import ggplot2
+#' @import tibble
+#' @import dplyr
+#' @import tidyr
 #' @export
 
 circular <- function(n=100, main_col="black", bg_col="white", s=56){
@@ -16,25 +20,25 @@ circular <- function(n=100, main_col="black", bg_col="white", s=56){
     output_mat[i,] <- rw_circular(n=n, p=0.5, lower_limit=10, mid_limit=90, upper_limit=100, seed=i*s)
   }
   colnames(output_mat) <- 1:n
-  plot_df <- tibble::tibble(val=1:360, tibble::as_tibble(output_mat))
-  plot_data <- tidyr::pivot_longer(plot_df, cols=2:(ncol(plot_df)))
-  p <- ggplot2::ggplot(plot_data, ggplot2::aes(x=val, y=name, group=val)) +
-    ggplot2::geom_line(colour=main_col) +
-    ggplot2::coord_polar(start = 0) +
-    ggplot2::theme(panel.background = ggplot2::element_rect(fill = bg_col, colour=bg_col),
-          plot.background = ggplot2::element_rect(fill = bg_col, colour=bg_col),
-          plot.title = ggplot2::element_blank(),
-          plot.subtitle = ggplot2::element_blank(),
+  plot_data <- tibble(val=1:360, tibble::as_tibble(output_mat)) %>%
+    pivot_longer(cols=2:(ncol(plot_df)))
+  p <- ggplot(plot_data, aes(x=val, y=name, group=val)) +
+    geom_line(colour=main_col) +
+    coord_polar(start = 0) +
+    theme(panel.background = element_rect(fill = bg_col, colour=bg_col),
+          plot.background = element_rect(fill = bg_col, colour=bg_col),
+          plot.title = element_blank(),
+          plot.subtitle = element_blank(),
           legend.position="none",
-          plot.margin = ggplot2::unit(c(0, 0, 0, 0), "cm"), #top, right, bottom, left
-          axis.title.x= ggplot2::element_blank(),
-          axis.title.y= ggplot2::element_blank(),
-          axis.text.x= ggplot2::element_blank(),
-          axis.text.y= ggplot2::element_blank(),
-          axis.ticks.x= ggplot2::element_blank(),
-          axis.ticks.y= ggplot2::element_blank(),
-          panel.grid.major = ggplot2::element_blank(),
-          panel.grid.minor = ggplot2::element_blank()
+          plot.margin = unit(c(0, 0, 0, 0), "cm"), #top, right, bottom, left
+          axis.title.x= element_blank(),
+          axis.title.y= element_blank(),
+          axis.text.x= element_blank(),
+          axis.text.y= element_blank(),
+          axis.ticks.x= element_blank(),
+          axis.ticks.y= element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank()
     )
   return(p)
 }
--- R/vortex.R
+++ R/vortex.R
@@ -8,6 +8,7 @@
 #' @param bg_col Background colour. Default "black".
 #' @param s Seed value. Default 1234.
 #' @return A ggplot object.
+#' @import ggplot2
 #' @export
 
 vortex <- function(n=25, start_val=90, col_scheme="mono", bg_col="black", s=1234){
@@ -21,24 +22,24 @@ vortex <- function(n=25, start_val=90, col_scheme="mono", bg_col="black", s=1234
   m <- n*10
   df1 <- data.frame(id=seq(1,m), value=sample(seq(0,m), m, replace=T), type=factor(rep(c(1:10),each=(m/10))))
   #plot
-  p <- ggplot2::ggplot() +
-    ggplot2::geom_line(data=df1, mapping=ggplot2::aes(x=id, y=value, group=type, colour=type)) +
-    ggplot2::scale_color_manual("", values=cols) +
-    ggplot2::coord_polar(start = start_val, theta="y") +
-    ggplot2::theme(panel.background = ggplot2::element_rect(fill = bg_col, colour=bg_col),
-                   plot.background = ggplot2::element_rect(fill = bg_col, colour=bg_col),
-                   plot.title = ggplot2::element_blank(),
-                   plot.subtitle = ggplot2::element_blank(),
+  p <- ggplot() +
+    geom_line(data=df1, mapping=aes(x=id, y=value, group=type, colour=type)) +
+    scale_color_manual("", values=cols) +
+    coord_polar(start = start_val, theta="y") +
+    theme(panel.background = element_rect(fill = bg_col, colour=bg_col),
+                   plot.background = element_rect(fill = bg_col, colour=bg_col),
+                   plot.title = element_blank(),
+                   plot.subtitle = element_blank(),
                    legend.position="none",
-                   plot.margin = ggplot2::unit(c(0, 0, 0, 0), "cm"), #top, right, bottom, left
-                   axis.title.x= ggplot2::element_blank(),
-                   axis.title.y= ggplot2::element_blank(),
-                   axis.text.x= ggplot2::element_blank(),
-                   axis.text.y= ggplot2::element_blank(),
-                   axis.ticks.x= ggplot2::element_blank(),
-                   axis.ticks.y= ggplot2::element_blank(),
-                   panel.grid.major = ggplot2::element_blank(),
-                   panel.grid.minor = ggplot2::element_blank()
+                   plot.margin = unit(c(0, 0, 0, 0), "cm"), #top, right, bottom, left
+                   axis.title.x= element_blank(),
+                   axis.title.y= element_blank(),
+                   axis.text.x= element_blank(),
+                   axis.text.y= element_blank(),
+                   axis.ticks.x= element_blank(),
+                   axis.ticks.y= element_blank(),
+                   panel.grid.major = element_blank(),
+                   panel.grid.minor = element_blank()
     )
   return(p)
 }
