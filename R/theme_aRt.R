8e46afa982fb3b24169d82bee143240c8ebe41ef
Nicola Rennie <nrennie35@gmail.com>
2022-03-19 17:09:15 +0000

    add custom theme

---
+++ R/theme_aRt.R
@@ -0,0 +1,10 @@
+#' aRt theme
+#'
+#' Custom ggplot2 theme for aRt objects
+#'
+#' @export
+#'
+
+theme_aRt <- ggplot2::theme_void() + #nolint
+    ggplot2::theme(legend.position = "none",
+                   plot.margin = ggplot2::unit(c(0, 0, 0, 0), "cm"))
