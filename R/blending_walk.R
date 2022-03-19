cc6ca63f69ee1918861475e370b266edbff46679
Nicola Rennie <nrennie35@gmail.com>
2022-03-19 17:16:32 +0000

    update docs

--- R/blending_walk.R
+++ R/blending_walk.R
@@ -5,7 +5,7 @@
 #' @param n Number of lines. Default 100.
 #' @param s Seed value. Default 1234.
 #' @return A numeric vector of length n
-#'
+#' @noRd
 
 blending_walk <- function(n = 100,
                           s = 1234) {
