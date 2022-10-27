039660ae731ac469d212230fde458342692b5052
nrennie <nrennie35@gmail.com>
2022-10-27 21:40:16 +0100

    move utils functions into main files

--- R/blending.R
+++ R/blending.R
@@ -1,3 +1,20 @@
+#' Blending Walk
+#'
+#' This function generates a random walk.
+#'
+#' @param n Number of lines. Default 100.
+#' @param s Seed value. Default 1234.
+#' @return A numeric vector of length n
+#' @noRd
+
+blending_walk <- function(n = 100,
+                          s = 1234) {
+  x <- stats::runif(n = n - 1)
+  x <- c(0, cumsum(x))
+  return(x)
+}
+
+
 #' Blending
 #'
 #' This function generates a generative art ggplot object using a random walk.
--- R/bubbles.R
+++ R/bubbles.R
@@ -1,3 +1,54 @@
+#' Draw Ellipse Circle
+#'
+#' This function generates data for an ellipse or circle.
+#'
+#' @param x0 x-cordinate of circle centre. Default 0.
+#' @param y0 y-cordinate of circle centre. Default 0.
+#' @param r vertical radius of circle. Default 5.
+#' @param a ratio of horizontal to vertical radii.
+#' @param n number of points to generate. Default 1000.
+#' @param group group to identify part of same circle. Default 1.
+#' @return a tibble
+#' @noRd
+#'
+
+draw_ellipse_circle <- function(x0 = 0,
+                                y0 = 0,
+                                r = 5, a = 1,
+                                n = 1000,
+                                group = 1) {
+  theta <- seq(0, 2 * pi, length.out = n)
+  tibble::tibble(x = a * r * cos(theta) + x0,
+                 y = r * sin(theta) + y0,
+                 group = rep(group, n))
+}
+
+#' Draw Ellipse in Circle
+#'
+#' This function generates data for a circle filled with ellipses of the same vertical radius
+#'
+#' @param x0 x-cordinate of circle centre. Default 0.
+#' @param y0 y-cordinate of circle centre. Default 0.
+#' @param r vertical radius of circle. Default 5.
+#' @param n number of points to generate. Default 1000.
+#' @return a tibble
+#' @noRd
+#'
+
+draw_ellipse_in_circle <- function(x0 = 0,
+                                   y0 = 0,
+                                   r = 5,
+                                   n = 1000) {
+  plot_data <- data.frame(x = c(), y = c(), group = c())
+  a <- seq(0, 1, 0.05)
+  for (i in seq_len(length(a))) {
+    k <- draw_ellipse_circle(x0 = x0, y0 = y0, r = r, a = a[i], n = n, group = i)
+    plot_data <- rbind(plot_data, k)
+  }
+  plot_data
+}
+
+
 #' Bubbles
 #'
 #' This function generates a generative art ggplot object consisting of circles filled with ellipses.
