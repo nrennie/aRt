63178d4a58598b68c7db38d14bc445a7b39fa6c8
nrennie <nrennie35@gmail.com>
2023-02-14 23:38:36 +0000

    move elevatr to suggests

--- R/contours.R
+++ R/contours.R
@@ -22,35 +22,40 @@ contours <- function(xmin = -3.27,
                      light = "white",
                      dark = "black",
                      range = c(0.5, 1)) {
-  # get elevation data
-  elev_data <- suppressMessages(elevatr::get_elev_raster(
-    locations = data.frame(x = c(xmin, xmax), y = c(ymin, ymax)),
-    z = 10,
-    prj = "EPSG:4326",
-    clip = "locations"))
-  # wrangling
-  plot_matrix <- raster::as.matrix(elev_data)
-  colnames(plot_matrix) <- seq_len(ncol(plot_matrix))
-  rownames(plot_matrix) <- seq_len(nrow(plot_matrix))
-  plot_data <- plot_matrix |>
-    tibble::as_tibble() |>
-    tibble::rownames_to_column(var = "y") |>
-    tidyr::pivot_longer(-c(.data$y), names_to = "x", values_to = "z") |>
-    dplyr::mutate(x = as.numeric(.data$x),
-                  y = as.numeric(.data$y))
-  # plot
-  p <- ggplot2::ggplot(data = plot_data,
-                  mapping = aes(x = .data$x,
-                                y = .data$y,
-                                z = .data$z)) +
-    metR::geom_contour_fill() +
-    metR::geom_contour_tanaka(light = light, dark = dark, range = range) +
-    ggplot2::scale_fill_gradientn(colours = col_palette) +
-    ggplot2::scale_y_reverse() +
-    ggplot2::coord_cartesian(expand = FALSE) +
-    ggplot2::theme_void() +
-    ggplot2::theme(legend.position = "none",
-                   plot.margin = ggplot2::margin(0, 0, 0, 0))
+  # check if {elevatr} loaded
+  if (!requireNamespace("elevatr")) {
+    stop("Please install {elevatr} to use this function.")
+  } else {
+    # get elevation data
+    elev_data <- suppressMessages(elevatr::get_elev_raster(
+      locations = data.frame(x = c(xmin, xmax), y = c(ymin, ymax)),
+      z = 10,
+      prj = "EPSG:4326",
+      clip = "locations"))
+    # wrangling
+    plot_matrix <- raster::as.matrix(elev_data)
+    colnames(plot_matrix) <- seq_len(ncol(plot_matrix))
+    rownames(plot_matrix) <- seq_len(nrow(plot_matrix))
+    plot_data <- plot_matrix |>
+      tibble::as_tibble() |>
+      tibble::rownames_to_column(var = "y") |>
+      tidyr::pivot_longer(-c(.data$y), names_to = "x", values_to = "z") |>
+      dplyr::mutate(x = as.numeric(.data$x),
+                    y = as.numeric(.data$y))
+    # plot
+    p <- ggplot2::ggplot(data = plot_data,
+                         mapping = aes(x = .data$x,
+                                       y = .data$y,
+                                       z = .data$z)) +
+      metR::geom_contour_fill() +
+      metR::geom_contour_tanaka(light = light, dark = dark, range = range) +
+      ggplot2::scale_fill_gradientn(colours = col_palette) +
+      ggplot2::scale_y_reverse() +
+      ggplot2::coord_cartesian(expand = FALSE) +
+      ggplot2::theme_void() +
+      ggplot2::theme(legend.position = "none",
+                     plot.margin = ggplot2::margin(0, 0, 0, 0))
 
-  return(p)
+    return(p)
+  }
 }
