50539bcfbdc95929e77b6b78fd18e85455700b34
nrennie <nrennie35@gmail.com>
2023-02-19 03:57:17 +0000

    feat: add gradients function

---
+++ R/gradients.R
@@ -0,0 +1,75 @@
+#' Generates a random hex code
+#'
+#' @param n Number of hex codes to generate
+#' @return Character string of hex codes
+random_hex <- function(n) {
+  generate_hex <- function() {
+    choices <- sample(c(as.character(0:9), LETTERS[1:6]), size = 6, replace = TRUE)
+    output <- paste0("#", stringr::str_flatten(choices))
+    return(output)
+  }
+  hex <- replicate(n = n, generate_hex(), simplify = TRUE)
+  return(hex)
+}
+
+#' Generates a tibble of line segments and colours
+#'
+#' @param xmin x-min coord
+#' @param xmax x-max coord
+#' @param ymin y-min coord
+#' @param ymax y-max coord
+#' @param low Hex value for left colour
+#' @param high Hex value for right colour
+#' @return Tibble
+make_square <- function(xmin, xmax, ymin, ymax, low, high) {
+  xvals <- seq(xmin, xmax, by = 0.001)
+  plot_data <- tibble::tibble(
+    x = xvals,
+    xend = xvals,
+    y = ymin,
+    yend = ymax,
+    colour = grDevices::colorRampPalette(c(low, high))(length(xvals))
+  )
+  return(plot_data)
+}
+
+#' Generates generative art as a grid of gradient colour fades
+#'
+#' @param nx Number of columns. Default 5.
+#' @param ny Number of rows. Default 5.
+#' @param s Random seed. Default 1234.
+#' @return Tibble
+#' @export
+
+gradients <- function(nx = 5,
+                      ny = 5,
+                      s = 1234) {
+  set.seed(s)
+  inputs <- tibble::tibble(
+    xmin = rep(seq_len(nx), each = ny),
+    xmax = rep(seq_len(nx) + 1, each = ny),
+    ymin = rep(seq_len(ny), times = nx),
+    ymax = rep(seq_len(ny) + 1, times = nx),
+    low = random_hex(nx * ny),
+    high = random_hex(nx * ny)
+  )
+
+  plot_data <- purrr::pmap_df(inputs, make_square)
+  ggplot2::ggplot(plot_data) +
+    ggplot2::geom_segment(
+      mapping = ggplot2::aes(
+        x = .data$x,
+        y = .data$y,
+        xend = .data$xend,
+        yend = .data$yend,
+        colour = I(.data$colour)
+      )
+    ) +
+    ggplot2::coord_fixed(expand = FALSE) +
+    ggplot2::theme_void() +
+    ggplot2::theme(legend.position = "none",
+                   plot.margin = ggplot2::margin(0, 0, 0, 0),
+                   plot.background = ggplot2::element_rect(fill = "transparent", colour = "transparent"),
+                   panel.background = ggplot2::element_rect(fill = "transparent", colour = "transparent"))
+
+}
