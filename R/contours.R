edf039169370a77fc0ce79428d17d1e2961e364a
nrennie <nrennie35@gmail.com>
2023-07-30 19:47:53 +0100

    fix: refactor contours

--- R/contours.R
+++ R/contours.R
@@ -7,10 +7,11 @@
 #' @param xmax Maximum x value og geographic area. Default -3.15.
 #' @param ymin Minimum y value og geographic area. Default 54.43.
 #' @param ymax Maximum y value og geographic area. Default 54.49.
-#' @param col_palette Colour palette to colour in contours. Default MetBrewer::met.brewer("Hokusai3").
-#' @param light Colour of light shading. Default "white".
-#' @param dark Colour of dark shading. Default "black".
-#' @param range Numeric vector of length 2 with the minimum and maximum size of lines. Default c(0.5, 1).
+#' @param col_palette Colour palette to colour in contours.
+#' Default MetBrewer::met.brewer("Hokusai3").
+#' @param res Resolution of elevation. Default 50.
+#' @param line_col Colour of contours. Default "#0a2e57".
+#' @param linewidth Width of contours. 0.5.
 #' @return A ggplot object.
 #' @export
 
@@ -19,47 +20,46 @@ contours <- function(xmin = -3.27,
                      ymin = 54.43,
                      ymax = 54.49,
                      col_palette = MetBrewer::met.brewer("Hokusai3"),
-                     light = "white",
-                     dark = "black",
-                     range = c(0.5, 1)) {
+                     res = 50,
+                     line_col = "#0a2e57",
+                     linewidth = 0.5) {
   # check if {elevatr} loaded
   if (!requireNamespace("elevatr", quietly = TRUE)) {
     stop("Please install {elevatr} to use this function.")
   } else {
-    if (!requireNamespace("metR", quietly = TRUE)) {
-      stop("Please install {metR} to use this function.")
-    } else {
-      # get elevation data
-      elev_data <- suppressMessages(elevatr::get_elev_raster(
-        locations = data.frame(x = c(xmin, xmax), y = c(ymin, ymax)),
-        z = 10,
-        prj = "EPSG:4326",
-        clip = "locations"))
-      # wrangling
-      plot_matrix <- raster::as.matrix(elev_data)
-      colnames(plot_matrix) <- seq_len(ncol(plot_matrix))
-      rownames(plot_matrix) <- seq_len(nrow(plot_matrix))
-      plot_data <- plot_matrix |>
-        tibble::as_tibble() |>
-        tibble::rownames_to_column(var = "y") |>
-        tidyr::pivot_longer(-c(.data$y), names_to = "x", values_to = "z") |>
-        dplyr::mutate(x = as.numeric(.data$x),
-                      y = as.numeric(.data$y))
-      # plot
-      p <- ggplot2::ggplot(data = plot_data,
-                           mapping = aes(x = .data$x,
-                                         y = .data$y,
-                                         z = .data$z)) +
-        metR::geom_contour_fill() +
-        metR::geom_contour_tanaka(light = light, dark = dark, range = range) +
-        ggplot2::scale_fill_gradientn(colours = col_palette) +
-        ggplot2::scale_y_reverse() +
-        ggplot2::coord_cartesian(expand = FALSE) +
-        ggplot2::theme_void() +
-        ggplot2::theme(legend.position = "none",
-                       plot.margin = ggplot2::margin(0, 0, 0, 0))
+    # get elevation data
+    elev_data <- suppressMessages(elevatr::get_elev_raster(
+      locations = data.frame(x = c(xmin, xmax), y = c(ymin, ymax)),
+      z = 10,
+      prj = "EPSG:4326",
+      clip = "locations"
+    ))
+    # get colours
+    min_val <- terra::minmax(elev_data)[1, 1]
+    max_val <- terra::minmax(elev_data)[2, 1]
+    break_vals <- seq(floor(min_val / res) * res, ceiling(max_val / res) * res, by = res)
+    cols <- grDevices::colorRampPalette(col_palette)(length(break_vals))
 
-      return(p)
-    }
+    # plot
+    p <- ggplot2::ggplot() +
+      tidyterra::geom_spatraster_contour_filled(
+        data = elev_data,
+        breaks = break_vals
+      ) +
+      tidyterra::geom_spatraster_contour(
+        data = elev_data,
+        breaks = break_vals,
+        colour = line_col,
+        linewidth = linewidth
+      ) +
+      ggplot2::scale_fill_manual(values = cols) +
+      ggplot2::coord_sf(expand = FALSE) +
+      ggplot2::theme_void() +
+      ggplot2::theme(
+        legend.position = "none",
+        plot.margin = ggplot2::margin(0, 0, 0, 0)
+      )
+
+    return(p)
   }
 }
